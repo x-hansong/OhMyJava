dab4a6bb63de085227b0d2f3e5a327217f786903
x-hansong <x.hansong.scut@gmail.com>
2016-02-07 17:06:51 +0800

    ContainsDuplicate

---
+++ src/main/java/com/easy/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package com.easy;
+
+import java.util.Arrays;
+
+/**
+ * Created by xhans on 2016/2/7.
+ */
+public class ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums){
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]){
+                return true;
+            }
+        }
+        return false;
+    }
+}
