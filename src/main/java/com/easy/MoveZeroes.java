25779681114485485b7fb2ed672cac0b64a73d73
x-hansong <x.hansong.scut@gmail.com>
2016-02-07 15:04:33 +0800

    MoveZeroes

---
+++ src/main/java/com/easy/MoveZeroes.java
@@ -0,0 +1,21 @@
+package com.easy;
+
+/**
+ * Created by xhans on 2016/2/7.
+ */
+public class MoveZeroes {
+    public void moveZeroes(int[] nums){
+        int count = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 0){
+                count++;
+            }else {
+                nums[i - count] = nums[i];
+            }
+        }
+
+        for (int i = 0; i < count; i++){
+            nums[nums.length - i - 1] = 0;
+        }
+    }
+}
