c2310bae10d5ec0cbb99fdff7980b3500632c120
x-hansong <x.hansong.scut@gmail.com>
2016-02-09 18:48:22 +0800

    Lowest Common Ancestor of a Binary Search Tree

---
+++ src/main/java/com/easy/LCA.java
@@ -0,0 +1,27 @@
+package com.easy;
+
+
+/**
+ * Created by xhans on 2016/2/9.
+ */
+public class LCA {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        } else {
+            return root;
+        }
+    }
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+}
