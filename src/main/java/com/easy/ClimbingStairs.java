2fd98843d4164aa67b8a72742bb9b108473f71d0
x-hansong <x.hansong.scut@gmail.com>
2016-02-12 14:43:05 +0800

    ClimbingStairs

---
+++ src/main/java/com/easy/ClimbingStairs.java
@@ -0,0 +1,16 @@
+package com.easy;
+
+/**
+ * Created by xhans on 2016/2/12.
+ */
+public class ClimbingStairs {
+    public int climbStairs(int n){
+        int[] mem = new int[n + 1];
+        mem[0] = 1;
+        mem[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            mem[i] = mem[i - 1] + mem [i - 2];
+        }
+        return mem[n];
+    }
+}
