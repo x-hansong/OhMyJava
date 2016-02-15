d8382713d6edb0e0d71e0d3ce67d525a9af075c3
x-hansong <x.hansong.scut@gmail.com>
2016-02-15 23:21:39 +0800

    Minimum Depth of Binary Tree

---
+++ src/main/java/com/easy/MinDepth.java
@@ -0,0 +1,18 @@
+package com.easy;
+
+import com.datastructure.TreeNode;
+
+/**
+ * Created by xhans on 2016/2/15.
+ */
+public class MinDepth {
+    public int minDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }else if (root.left == null || root.right == null){
+            return 1 + Math.max(minDepth(root.left), minDepth(root.right));
+        } else {
+            return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+        }
+    }
+}
