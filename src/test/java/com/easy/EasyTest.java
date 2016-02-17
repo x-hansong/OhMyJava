78d2fbab35b3408fc9fb8331299556b201ba95f4
x-hansong <x.hansong.scut@gmail.com>
2016-02-17 21:01:04 +0800

    BullsAndCows

--- src/test/java/com/easy/EasyTest.java
+++ src/test/java/com/easy/EasyTest.java
@@ -138,10 +138,17 @@ public class EasyTest {
         Assert.assertEquals(16, rectangleArea.computeArea(-2, -2, 2, 2, -2, -2, 2, 2));
     }
 
-    @Test
+//    @Test
     public void testContainsDuplicate2(){
         ContainsDuplicate2 c = new ContainsDuplicate2();
 
         Assert.assertTrue(c.containsNearbyDuplicate(new int[] {1, 0, 1, 1}, 1));
+    }
+
+    @Test
+    public void testBullsAndCows(){
+        BullsAndCows bullsAndCows = new BullsAndCows();
+
+        bullsAndCows.getHint("1122", "1222");
     }
 }
