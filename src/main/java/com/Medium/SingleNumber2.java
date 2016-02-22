170115e10ad5d1f497d34db607a6053492b972bf
x-hansong <x.hansong.scut@gmail.com>
2016-02-22 22:24:41 +0800

    Single Number II

---
+++ src/main/java/com/Medium/SingleNumber2.java
@@ -0,0 +1,24 @@
+package com.Medium;
+
+/**
+ * Created by xhans on 2016/2/22.
+ */
+public class SingleNumber2 {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int bit = 1 << i;
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if ((bit & nums[j]) != 0){
+                    sum++;
+                }
+            }
+            if (sum % 3 == 1){
+                result |= bit;
+            }
+        }
+
+        return result;
+    }
+}
