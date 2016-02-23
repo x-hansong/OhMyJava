4e53ebcb9f3937b665c265779d404c022e399b40
x-hansong <x.hansong.scut@gmail.com>
2016-02-23 13:07:05 +0800

    Product of Array Except Self

--- src/main/java/com/Medium/SingleNumber2.java
+++ src/main/java/com/Medium/SingleNumber2.java
@@ -9,6 +9,7 @@ public class SingleNumber2 {
         for (int i = 0; i < 32; i++) {
             int bit = 1 << i;
             int sum = 0;
+            //记录每一位1的个数
             for (int j = 0; j < nums.length; j++) {
                 if ((bit & nums[j]) != 0){
                     sum++;
