0b3376dbd48bc66cde1c5ab5646830f3c0f90bed
x-hansong <x.hansong.scut@gmail.com>
2016-03-24 11:26:46 +0800

    Hashmap

---
+++ src/main/java/com/java/collections/HashMapTest.java
@@ -0,0 +1,36 @@
+package com.java.collections;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by xhans on 2016/3/23.
+ */
+public class HashMapTest {
+    static class MyOject{
+        private int i;
+
+        MyOject(int i) {
+            this.i = i;
+        }
+
+        public int getI() {
+            return i;
+        }
+
+        public void setI(int i) {
+            this.i = i;
+        }
+    }
+    public static void main(String[] args){
+        HashMap<MyOject, Integer> map = new HashMap<>();
+        for (int i = 0; i < 10; i++) {
+            map.put(new MyOject(i), i);
+        }
+        for (Map.Entry<MyOject, Integer> entry : map.entrySet()){
+            MyOject i = entry.getKey();
+            i.setI(10);
+        }
+        System.out.println(map.get(new MyOject(10)));
+    }
+}
